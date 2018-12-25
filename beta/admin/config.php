199c0dd7c24b4bff112cf3f184b835da7466afac
mkabalane <mkabalane@gmail.com>
2018-12-25 16:58:21 +0200

    v3

--- beta/admin/config.php
+++ beta/admin/config.php
@@ -1,6 +1,6 @@
 <?php
 
- $con = mysqli_connect("localhost", "d2lwhlc5s5dc", "e00gO%N(J/=<", "otlobOnline");
+ $con = mysqli_connect("localhost", "d2lwhlc5s5dc", "js+ON6l(H", "otlobOnline");
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
